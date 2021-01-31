975ae3be68dfeb53c0a8923d92a58ac536012bb3
Jeff-King <szwjf08@gmail.com>
2021-01-31 22:01:56 +0800

    use unsigned body in `canonical_request` when the body is not required to be signed

--- R/canonical_request.R
+++ R/canonical_request.R
@@ -17,14 +17,14 @@
 #' # http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
 #' fromDocs <- "POST
 #' /
-#' 
+#'
 #' content-type:application/x-www-form-urlencoded; charset=utf-8
 #' host:iam.amazonaws.com
 #' x-amz-date:20110909T233600Z
-#' 
+#'
 #' content-type;host;x-amz-date
 #' b6359072c78d70ebee1e81adcbab4f01bf2c23245fa365ef83fe8f1f955085e2"
-#' 
+#'
 #' hdrs <- list(`Content-Type` = "application/x-www-form-urlencoded; charset=utf-8",
 #'              Host = "iam.amazonaws.com",
 #'              `x-amz-date` = "20110909T233600Z")
@@ -33,11 +33,11 @@
 #'                        query_args = list(),
 #'                        canonical_headers = hdrs,
 #'                        request_body = "Action=ListUsers&Version=2010-05-08")
-#' 
+#'
 #' identical(fromDocs, r$canonical)
 #' @seealso \code{link{signature_v4_aut}}, \code{\link{string_to_sign}}
 #' @export
-canonical_request <- 
+canonical_request <-
 function(verb,
          canonical_uri = "",
          query_args = list(),
@@ -50,16 +50,19 @@ function(verb,
     } else {
         body_hash <- tolower(digest::digest(request_body, algo = "sha256", serialize = FALSE))
     }
-    
+
     # set sort locale
     lc <- Sys.getlocale(category = "LC_COLLATE")
     Sys.setlocale(category = "LC_COLLATE", locale = "C")
     on.exit(Sys.setlocale(category = "LC_COLLATE", locale = lc))
-    
+
     if (isTRUE(signed_body)) {
         canonical_headers[['x-amz-content-sha256']] = body_hash
+        body_text <- body_hash
+    }else{
+        body_text <- request_body
     }
-    
+
     names(canonical_headers) <- tolower(names(canonical_headers))
     canonical_headers <- canonical_headers[order(names(canonical_headers))]
     # trim leading, trailing, and all non-quoted duplicated spaces
@@ -68,20 +71,20 @@ function(verb,
     signed_headers <- paste(names(canonical_headers), sep = "", collapse = ";")
     if(length(query_args)) {
         query_args <- unlist(query_args[order(names(query_args))])
-        a <- paste0(sapply(names(query_args), URLencode, reserved = TRUE), "=", 
+        a <- paste0(sapply(names(query_args), URLencode, reserved = TRUE), "=",
                     sapply(as.character(query_args), URLencode, reserved = TRUE))
         query_string <- paste(a, sep = "", collapse = "&")
     } else {
         query_string <- ""
     }
-    out <- paste(verb, 
+    out <- paste(verb,
                  canonical_uri,
                  query_string,
                  header_string,
                  signed_headers,
-                 body_hash,
+                 body_text,
                  sep = "\n")
-    return(list(headers = signed_headers, 
+    return(list(headers = signed_headers,
                 body = body_hash,
                 canonical = out,
                 hash = digest::digest(out, algo = "sha256", serialize = FALSE)))
